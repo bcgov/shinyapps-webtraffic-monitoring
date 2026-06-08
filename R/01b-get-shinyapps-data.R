d1b4c59095f369b2ece11b637b57f69cb991c5df
Anakin2009 <kcjzj007@gmail.com>
2026-06-08 13:36:50 -0700

    Add Apache 2.0 license header and format saveRDS call in shinyapps data script

--- R/01b-get-shinyapps-data.R
+++ R/01b-get-shinyapps-data.R
@@ -1,3 +1,17 @@
+# Copyright 2026 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Purpose: Download shinyapps.io metrics (concurrency) and save locally
 
 source("R/00-setup.R")
@@ -97,4 +111,7 @@ all_concurrency <- expand_grid(
   select(data) |>
   unnest(data)
 
-saveRDS(all_concurrency, file = file.path(DATA_RAW, "shinyapps_concurrency.rds"))
+saveRDS(
+  all_concurrency,
+  file = file.path(DATA_RAW, "shinyapps_concurrency.rds")
+)
